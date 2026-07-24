04457e74a7b28f06a79dcbf8bea2df12ffb4f679
Mostafa Hafezypoor <46339325+mostafaHafezyPoor20@users.noreply.github.com>
2026-07-24 05:58:27 +0330

    Update Constants.java
    change configuration infroramtion

--- app/src/main/java/mostafa/hafezypoor/ahmmad/panel/utils/Constants.java
+++ app/src/main/java/mostafa/hafezypoor/ahmmad/panel/utils/Constants.java
@@ -1,7 +1,7 @@
 package mostafa.hafezypoor.ahmmad.panel.utils;
 
 public class Constants {
-    public static String SERVER_URL="https://boskabadi3d.ir/app/Controllers/API/Admin/";
-    public static String key="iNdTTvoVOGAdFc7HuYNCJWFSqct0ICre8Lscq24INoeJFKCct1PPGCdxG3CA4foj4HWiWwP6KBbe8rtkASBSGDuoJ9s9vNq1OApmWBEYRwU7DfYrLbZej4xhRNz4aIjuN86MPsfSsNfhjoIcNqqDK4";
+    public static String SERVER_URL="https://YOUR_SERVER_URL/app/Controllers/API/Admin/";
+    public static String key="AUTONTICATION KEY";
 
 }
